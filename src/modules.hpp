0879c846acc131194192b1ff2660e7827bae9a3e
Nicolas-Py <nicolasmallinckrodt@gmail.com>
2024-06-25 21:21:16 +0200

    Setup test envo

--- src/modules.hpp
+++ src/modules.hpp
@@ -9,8 +9,29 @@
 #include "systemc.h"
 using namespace sc_core;
 
-extern "C" int run_simulation(int seconds);
+struct Request {
+     uint32_t addr ;
+     uint32_t data ;
+     int we ;
+};
 
+struct Result {
+	size_t cycles;
+ 	size_t misses;
+ 	size_t hits;
+ 	size_t primitiveGateCount;
+};
 
+extern "C" struct Result run_simulation (
+	int cycles,
+	int directMapped,
+	unsigned cacheLines,
+	unsigned cacheLineSize,
+	unsigned cacheLatency,
+	unsigned memoryLatency,
+	size_t numRequests,
+	Request requests[numRequests],
+	const char * tracefile
+);
 
 #endif
