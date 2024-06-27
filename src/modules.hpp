2717a287541efb62e5e9034d0fa216a5375a37a1
tofusaladd <ziangnam@gmail.com>
2024-06-27 23:20:32 +0200

    Update modules.hpp

--- src/modules.hpp
+++ src/modules.hpp
@@ -9,29 +9,55 @@
 #include "systemc.h"
 using namespace sc_core;
 
-struct Request {
-     uint32_t addr ;
-     uint32_t data ;
-     int we ;
+extern "C" {
+
+    struct Request {
+        uint32_t addr;
+        uint32_t data;
+        int we; // 0 for read, 1 for write
+    };
+
+    struct Result {
+        size_t cycles;
+        size_t misses;
+        size_t hits;
+        size_t primitiveGateCount;
+    };
+
+    struct Result run_simulation(
+            int cycles,
+            int directMapped, // 0 for full-associative, 1 for direct mapped
+            unsigned cacheLines,
+            unsigned cacheLineSize,
+            unsigned cacheLatency,
+            unsigned memoryLatency,
+            size_t numRequests,
+            Request* requests[numRequests],
+            const char* traceFile
+            );
+}
+
+SC_MODULE(CacheLine) {
+    //struct for cache line, which include tag, index, and offset bits
+
 };
 
-struct Result {
-	size_t cycles;
- 	size_t misses;
- 	size_t hits;
- 	size_t primitiveGateCount;
+SC_MODULE(Cache) {
+    sc_in<bool> clk;
+
+    //inputs
+    sc_in<sc_vector<uint32_t>> addr;
+    sc_in<sc_vector<uint32_t>> data_in; //for write
+
+    //outputs
+    sc_out<sc_vector<uint32_t>> data_out; //for read
+    sc_out<bool> hits; // 0 for miss, 1 for hit
+    
+
+    void process() {
+
+    }
 };
 
-extern "C" struct Result run_simulation (
-	int cycles,
-	int directMapped,
-	unsigned cacheLines,
-	unsigned cacheLineSize,
-	unsigned cacheLatency,
-	unsigned memoryLatency,
-	size_t numRequests,
-	Request requests[numRequests],
-	const char * tracefile
-);
 
 #endif
