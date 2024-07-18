cd208d47c12d1aeb7a167c2eeb172e8aead54d62
ziang <ziangnam@gmail.com>
2024-07-18 22:00:36 +0200

    added full associative

--- src/modules.cpp
+++ src/modules.cpp
@@ -3,6 +3,7 @@
 //
 #include "modules.hpp"
 #include "direct-mapped-cache.hpp"
+#include "full_associative_cache.hpp"
 #include "main-memory.hpp"
 # include <map>
 
@@ -51,6 +52,38 @@ Result run_simulation (
 		}
 		Result testResult = {cyclesCount, misses, hits, 1};
 		return testResult;
+	} else {
+
+		ASSOCIATIVE_CACHE associative_cache("associative", cacheLines, cacheLineSize, sizeof(uint32_t));
+		sc_signal<MEMORY_REQUEST> request;
+		sc_signal<int> hit;
+
+		size_t misses = 0;
+		size_t hits = 0;
+		size_t cyclesCount = 0;
+
+		associative_cache.request(request);
+		associative_cache.out(hit);
+
+		for (int i = 0; i < numRequests; i++) {
+			printf("Request number: %u, address: %u, data: %u, write/enable: %d\n", i + 1, requests[i].addr, requests[i].data, requests[i].we);
+			request.write(MEMORY_REQUEST{requests[i].addr, requests[i].data, requests[i].we, i});
+			sc_start(1, SC_SEC);
+			if (hit.read()) {
+				hits++;
+				cyclesCount += cacheLatency;
+			} else {
+				misses++;
+				cyclesCount += cacheLatency;
+				cyclesCount += memoryLatency;
+			}
+			if (cyclesCount>cycles) {
+				cyclesCount = SIZE_MAX;
+				break;
+			}
+			Result testResult = {cyclesCount, misses, hits, 1};
+			return testResult;
+		}
 	}
 }
 
