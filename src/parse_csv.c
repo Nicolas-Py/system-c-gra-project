6c36f724785af4301ec28ba6004dc2c7f924ea33
ziang <ziangnam@gmail.com>
2024-07-20 19:14:16 +0200

    updating parsing, prepare for merge

--- src/parse_csv.c
+++ src/parse_csv.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include "parse_csv.h"
 
+#include <errno.h>
 
 
 size_t line_count(const char* path) {
@@ -30,6 +31,17 @@ size_t line_count(const char* path) {
 
 }
 
+int is_valid(const char* str) {
+    errno = 0;
+    strtoul(str, NULL, 0);
+    if (errno != 0) {
+        return 0; // Invalid number
+    }
+    return 1; // Valid number
+}
+
+
+
 struct Request* parse_csv(const char* path, size_t* num_requests) {
 
     FILE* file = fopen(path, "r");
@@ -43,6 +55,7 @@ struct Request* parse_csv(const char* path, size_t* num_requests) {
 
     if (requests == NULL) {
         perror("Failed to allocate memory");
+        fclose(file);
         exit(EXIT_FAILURE);
     }
 
@@ -57,20 +70,33 @@ struct Request* parse_csv(const char* path, size_t* num_requests) {
 
         if (operation == NULL || addr_str == NULL) {
             fprintf(stderr, "Invalid CSV format at line %zu\n", index + 1);
-            continue; // Skip invalid lines
+            return NULL;
         }
 
         if (strcmp(operation, "W") == 0) {
             requests[index].we = 1;
+            if (data_str == NULL || !is_valid(data_str)) {
+                fprintf(stderr, "Invalid data or missing data for write operation at line %zu\n", index + 1);
+                return NULL;
+            }
+            requests[index].data = (uint32_t)strtoul(data_str, NULL, 0);
         } else if (strcmp(operation, "R") == 0) {
             requests[index].we = 0;
+            if (data_str != NULL && strlen(data_str) > 0) {
+                fprintf(stderr, "Data provided for read operation at line %zu, should be empty!\n", index + 1);
+                return NULL;
+            }
+            requests[index].data = 0;
         } else {
             fprintf(stderr, "Unknownn operation '%s' at line %zu\n", operation, index + 1);
+            return NULL;
         }
 
-
+        if (!is_valid(addr_str)) {
+            fprintf(stderr, "Invalid address '%s' at line %zu\n", addr_str, index + 1);
+            return NULL;
+        }
         requests[index].addr = (uint32_t)strtoul(addr_str, NULL, 0);
-        requests[index].data = (data_str) ? (uint32_t)strtoul(data_str, NULL, 0) : 0;
         index++;
     }
 
